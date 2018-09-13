449686ce72e54f8e920d70ab083cfca960125f20
sergeydaub <sergeydaub@gmail.com>
2018-09-13 11:06:30 +0300

    -- refactoring LingvoApi

--- dictionaries/lingvo/api.js
+++ dictionaries/lingvo/api.js
@@ -6,7 +6,7 @@ const API_KEY = 'MzA1OTMzZjktNzY3OC00MTM4LTkzNTQtOWFiZjRhNWNiY2NkOjhjZmYwZTAyYWY
 
 export default class LingvoApi {
     constructor() {
-        this.token = 'Z342'; //null; // 'ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmxlSEFpT2pFMU1URXhNelkxTlRNc0lrMXZaR1ZzSWpwN0lrTm9ZWEpoWTNSbGNuTlFaWEpFWVhraU9qVXdNREF3TENKVmMyVnlTV1FpT2pRME1pd2lWVzVwY1hWbFNXUWlPaUl6TURVNU16Tm1PUzAzTmpjNExUUXhNemd0T1RNMU5DMDVZV0ptTkdFMVkySmpZMlFpZlgwLkpnS1l1R2l3LWFzSnZZeEtscUhySFdHV0x2b1dNMDB6cDZ4VVd4MERjNDA='
+        this.token = null; // 'ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmxlSEFpT2pFMU1URXhNelkxTlRNc0lrMXZaR1ZzSWpwN0lrTm9ZWEpoWTNSbGNuTlFaWEpFWVhraU9qVXdNREF3TENKVmMyVnlTV1FpT2pRME1pd2lWVzVwY1hWbFNXUWlPaUl6TURVNU16Tm1PUzAzTmpjNExUUXhNemd0T1RNMU5DMDVZV0ptTkdFMVkySmpZMlFpZlgwLkpnS1l1R2l3LWFzSnZZeEtscUhySFdHV0x2b1dNMDB6cDZ4VVd4MERjNDA='
     }
 
     /**
@@ -14,21 +14,22 @@ export default class LingvoApi {
      * @param {string} text Переводимый текст
      * @param {string} langFrom Двухсимвольное обозначение исходного языка
      * @param {string} langTo Двухсимвольное обозначение целевого языка
-     * @param {boolean} [forceGetToken=false]
      * @returns {Promise<string>} Промис результата перевода
      */
-    translate(text, langFrom = 'de', langTo = 'ru', forceGetToken = false) {
-        return getToken.call(this, forceGetToken)
-            .then(token => translate.call(this, token, text, langFrom, langTo))
-            .catch(err => {
-                if (err.code === 401) {
-                    console.info(`${err.message}. Attempting get fresh token.`)
-                    return this.translate( text, langFrom, langTo, true)
-                } else {
-                    console.error(err.message)
-                    throw err
-                }
-            })
+    translate(text, langFrom = 'de', langTo = 'ru') {
+      return requestByKey('Translation', getToken.bind(this), text, langFrom, langTo)
+    }
+
+    miniCard(text, langFrom = 'de', langTo = 'ru') {
+      return requestByKey('Minicard', getToken.bind(this), text, langFrom, langTo)
+    }
+
+    sound(text, langFrom = 'de', langTo = 'ru') {
+      return this.miniCard(text, langFrom, langTo)
+        .then(data => {
+          const { Translation: { DictionaryName , SoundName } } = data && JSON.parse(data) || {Translation: {}}
+          return {dictionaryName : DictionaryName, soundName: SoundName}
+        })
     }
 }
 
@@ -59,38 +60,13 @@ function getToken(forceGetToken = false) {
     })
 }
 
-function translate(token, text, langFrom = 'de', langTo = 'ru') {
-    return new Promise((success, reject) => {
-            const xhr = new XMLHttpRequest()
-            xhr.withCredentials = true
+function requestByKey(key, getToken, text, langFrom = 'de', langTo = 'ru'){
+  return request(getToken, key, {
+    text,
+    srcLang: getLanguageCode(langFrom),
+    dstLang: getLanguageCode(langTo),
+  })
 
-            xhr.addEventListener("readystatechange", function () {
-                if (this.readyState === 4) {
-                    console.log(this.responseText)
-                }
-            })
-
-            xhr.open("GET", encodeURI(`${SERVICE_URL}/api/v1/Translation?text=${text}&srcLang=${getLanguageCode(langFrom)}&dstLang=${getLanguageCode(langTo)}`))
-            xhr.setRequestHeader("authorization", `Bearer ${token}`)
-            xhr.setRequestHeader("cache-control", "no-cache")
-
-            xhr.send(null)
-
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState !== 4) return
-
-                if (xhr.status === 200) {
-                    success(xhr.responseText) }
-                else if (xhr.status === 401) {
-                    const err = new Error('Token is expired')
-                    err.code = 401
-                    reject(err)
-                } else {
-                    reject(new Error(`Ошибка при получении перевода: ${xhr.status}`))
-                }
-            }
-        }
-    )
 }
 
 function getLanguageCode(langauge) {
@@ -106,6 +82,66 @@ function getLanguageCode(langauge) {
     return code
 }
 
+/**
+ *
+ * @param {function} getToken
+ * @param key
+ * @param params
+ * @param forceGetToken
+ * @this {LingvoApi}
+ * @returns {Promise<any>}
+ */
+function request(getToken, key, params, forceGetToken = false){
+  return getToken(forceGetToken)
+    .then(token => requestWithToken(token, key, params))
+    .catch(err => {
+      if (err.code === 401) {
+        console.info(`${err.message}. Attempting get fresh token.`)
+        return request(getToken, key, params, true)
+      } else {
+        console.error(err.message)
+        throw err
+      }
+    })
+}
+
+function requestWithToken(token, key, params) {
+  return new Promise((success, reject) => {
+      const xhr = new XMLHttpRequest()
+      xhr.withCredentials = true
+
+      // xhr.addEventListener("readystatechange", function () {
+      //   if (this.readyState === 4) {
+      //     console.log(this.responseText)
+      //   }
+      // })
+      //
+      xhr.open("GET", encodeURI(makeApiURI(key, params)))
+      xhr.setRequestHeader("authorization", `Bearer ${token}`)
+      xhr.setRequestHeader("cache-control", "no-cache")
+
+      xhr.send(null)
+
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState !== 4) return
+
+        if (xhr.status === 200) {
+          success(xhr.responseText) }
+        else if (xhr.status === 401) {
+          const err = new Error('Token is expired')
+          err.code = 401
+          reject(err)
+        } else {
+          reject(new Error(`Ошибка при получении перевода: ${xhr.status}`))
+        }
+      }
+    })
+}
+
+function makeApiURI(key, params) {
+  const query = Object.keys(params).map(k => `${k}=${params[k]}`).join('&')
+  return `${SERVICE_URL}/api/v1/${key}${query && '?' + query}`
+}
 
 // const api = new LingvoApi()
 // api.translate('machen', 'de', 'ru')
