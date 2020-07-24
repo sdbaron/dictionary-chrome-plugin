8f681da433032d316184f0eb0cf4e05a1702990e
sergeydaub <sergeydaub@gmail.com>
2020-07-24 20:03:15 +0300

    -- up state from language Selection

--- display/langSelector/langSelector.jsx
+++ display/langSelector/langSelector.jsx
@@ -1,50 +1,25 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
-const langOptionsDefs = {
-  'ru': 'Русский',
-  'de': 'German',
-  'en': 'English'
-}
 
 class LangSelector extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { lang: props.lang || 'de' }
-    this.onChange = this.onChange.bind(this)
-  }
-
-  onChange(e) {
-    Object.keys(langOptionsDefs).some((key, ind) => {
-      if (ind === e.selectedIndex) {
-        this.setState({ lang: e.selectedIndex })
-        return true
-      }
-    })
   }
 
   render() {
-    const options = Object.entries(langOptionsDefs)
+    const options = Object.entries(this.props.langOptionsDefs)
       .map(([value, name], index) =>
-        (<option key={index} value={value}
+        (<option key={value} value={value}
                         className="">{name}</option>)
       )
 
     return (
-      <select className="sdb-popup-card-lang-selector" onChange={this.onChange}
-              defaultValue={this.state.lang} >
+      <select className="sdb-popup-card-lang-selector" onChange={this.props.onChange}
+              defaultValue={this.props.lang} >
         {options}
       </select>
     )
   }
 
 }
-
-// function render(rootElement, data) {
-//   ReactDOM.render(
-//     <LangSelector name={data.name} lang={ data.lang } />,
-//     rootElement
-//   )
-// }
 
 export default LangSelector
--- display/langSelector/pairLangSelector.jsx
+++ display/langSelector/pairLangSelector.jsx
@@ -3,25 +3,52 @@ import ReactDOM from 'react-dom'
 
 import LangSelector from './langSelector'
 
+const langOptionsDefs = {
+  'ru': 'Русский',
+  'de': 'German',
+  'en': 'English'
+}
+
 class PairLangSelector extends React.Component {
   constructor(props) {
     super(props)
     const { srcLng, tgtLng } = props
-    this.state = { srcLng, tgtLng }
-    this.onChange = this.onChange.bind(this)
+    this.state = {
+      srcLng,
+      tgtLng
+    }
+    this.onSrcChange = this.onSrcChange.bind(this)
+    this.onTgtChange = this.onTgtChange.bind(this)
+
   }
 
-  onChange(e) {
+  onSrcChange(e) {
+    this.setState({ srcLng: e.nativeEvent.target.selectedOptions[0].value })
+  }
+
+  onTgtChange(e) {
+    this.setState({ tgtLng: e.nativeEvent.target.selectedOptions[0].value } )
   }
 
   render() {
+
+    const { srcLng, tgtLng } = this.state
+    const srcLangOptions = langOptionsDefs
+      // .reduce((res, [key, value]) => {
+      //   key !== tgtLng && (res[key] = value); return res }, {})
+
+    const tgtLangOptions = Object.entries(langOptionsDefs)
+      .reduce((res, [key, value]) => {
+        key !== srcLng && (res[key] = value); return res }, {})
+
     return <div>
-      <LangSelector name="srcLng" lang={this.state.srcLng}/>
-      <LangSelector name="tgtLng" lang={this.state.tgtLng}/>
+      <LangSelector name="srcLng" lang={this.state.srcLng} onChange={this.onSrcChange} langOptionsDefs={srcLangOptions} />
+      <LangSelector name="tgtLng" lang={this.state.tgtLng} onChange={this.onTgtChange} langOptionsDefs={tgtLangOptions}/>
     </div>
   }
 
 }
+
 
 function render(rootElement, data) {
   ReactDOM.render(
