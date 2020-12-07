4bdf4ec237fa6b5e9b159233348f251f43d6f8fc
sergeydaub <sergeydaub@gmail.com>
2020-12-07 21:19:32 +0300

    feat(auth): available set and get sessionId on the standalone iframe http://lancinate-meridian.000webhostapp.com/

--- auth/iframe/index.js
+++ auth/iframe/index.js
@@ -3,24 +3,24 @@ console.log('iframe start')
 
 function getMessage(event) {
   const { origin, data } = event || {}
-  console.log(`IFRAME getMessage() origin=${origin} data=${data}`)
+  console.log(`IFRAME getMessage() origin=${origin} data=${data && JSON.stringify(data, null, 2)}`)
   const { action, value } = data
   switch (action) {
     case 'getSessionId':
       setTimeout(() => {
+
         const pairs = document.cookie.split(';')
-        pairs && pairs.forEach(p => {
-          const [name, val] = p.split('=')
-          if (name === 'sid') {
-            window.parent.postMessage({ sessionId: val }, '*')
-          }
-        })
+        let sessionId = getSession()
+        if (!sessionId && value === true) {
+          sessionId = (Math.random() * 10000).toString(16)
+          setSession(sessionId)
+        }
+        window.parent.postMessage({ sessionId }, '*')
       })
       break;
     case 'setSessionId':
-      value && (document.cookie = `sid=${value}`)
+      setSession(value)
       break
-
   }
 }
 
@@ -28,15 +28,27 @@ function onPageLoad() {
   window.addEventListener('message', getMessage)
   window.parent.postMessage('', '*')
   window.parent.postMessage('Hello from inside iframe', '*')
+  console.log('iframe onPageLoad')
 }
 
 if (window.document.readyState === 'complete') onPageLoad()
 else window.addEventListener('load', onPageLoad)
 
-function getSession() {
+const SessionIdName = 'sid'
 
+function getSession() {
+  const pairs = document.cookie.split(';')
+  let sessionId
+  pairs && pairs.some(p => {
+    const [name, val] = p.split('=')
+    if (name.trim() === SessionIdName) {
+      sessionId = val
+      return true
+    }
+  })
+  return sessionId
 }
 
-function setSession(data) {
-
+function setSession(value) {
+  value && (document.cookie = `${SessionIdName}=${value}; SameSite=None;`)
 }
